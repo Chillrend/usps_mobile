e15756d4e30493e57a4a586a8861525bd71cc5db
Farhan Hanif <cv2lex@gmail.com>
2018-11-23 10:07:49 +0700

    Changed using Material Design

--- app/src/main/java/org/chillrend/brieftrager/home.java
+++ app/src/main/java/org/chillrend/brieftrager/home.java
@@ -8,15 +8,24 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import java.util.HashMap;
 
 public class home extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
+    private SliderLayout mSlider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,8 +64,30 @@ public class home extends AppCompatActivity {
             }
         });
 
-    }
+        mSlider = (SliderLayout) findViewById(R.id.slider);
 
+        HashMap<String,String> urlMaps = new HashMap<String, String>();
+        urlMaps.put("demo","http://192.168.43.14/brieftrager/header.jpg");
+        urlMaps.put("demo","http://192.168.43.14/brieftrager/header.jpg");
+        urlMaps.put("demo","http://192.168.43.14/brieftrager/header.jpg");
+        urlMaps.put("demo","http://192.168.43.14/brieftrager/header.jpg");
+
+        for(String name : urlMaps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(this);
+
+            textSliderView.description(name).image(urlMaps.get(name)).setScaleType(BaseSliderView.ScaleType.Fit);
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle().putString("extra",name);
+
+            mSlider.addSlider(textSliderView);
+        }
+
+    }
+    @Override
+    protected void onStop(){
+        mSlider.stopAutoCycle();
+        super.onStop();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch(item.getItemId()){
